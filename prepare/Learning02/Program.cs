f5cfecacb61db19c4e49a82c5945e180a007a854
mikelroper <122181874+mikelroper@users.noreply.github.com>
2023-09-24 14:32:17 -0600

    updates.

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Juicy";
+        job1._jobTitle = "Senior Juicer";
+        job1._startYear = 2020;
+        job1._endYear = 2021;
+        job1.Display();
+
+        Job job2 = new Job();
+        job2._company = "Fried Chicken";
+        job2._jobTitle = "Lead Chicken maker";
+        job2._startYear = 2021;
+        job2._endYear = 2022;
+        job2.Display();
+
+        Resume myResume = new Resume();
+        myResume._name = "John Jacob Jingle-Heimer-Schmidt";
+
+        myResume._job.Add(job1);
+        myResume._job.Add(job2);
+
+        myResume.Display();
     }
+
+
 }
---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+public class Resume
+{
+    public string _name;
+    public List<Job> _job = new List<Job>();
+    
+    public void Display()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs:");
+        foreach (var job in _job)
+        {
+            job.Display();
+        }
+    }
+
+
+}
